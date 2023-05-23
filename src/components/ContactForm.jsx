f02bcc0283e45ec11308d6aa5d67d32e7f3da7e7
Dhruvin Khant - DK <dhruvink.spaceo@gmail.com>
2023-05-23 23:27:41 +0530

    search feature added

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,155 +6,94 @@ import { addItem, removeAllItem, removeSingleItem } from '../reduxToolkit/slices
 const ContactForm = () => {
   const dispatch = useDispatch();
   const formData = useSelector((state) => state.formdata);
-  const todoitems = useSelector((state) => state.todo)
+  const todoitems = useSelector((state) => state.todo);
   const [itemvalue, setValue] = useState("");
-  // const [formData, setFormData] = useState({
-  //   textInput: '',
-  //   emailInput: '',
-  //   passwordInput: '',
-  //   checkboxInput: false,
-  //   radioInput: '',
-  //   selectInput: '',
-  //   textareaInput: '',
-  // });
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleChange = (event) => {
-    // const { name, value, type, checked } = event.target;
-    // const updatedValue = type === 'checkbox' ? checked : value;
-
-    // setFormData((prevFormData) => ({
-    //   ...prevFormData,
-    //   [name]: updatedValue,
-    // }));
     dispatch(updateFormData(event.target));
   };
 
   const handleAddItem = () => {
     if (itemvalue === "") {
-      alert('Please enter the value')
+      alert('Please enter a value');
     } else {
       dispatch(addItem(itemvalue));
-      setValue("")
+      setValue("");
     }
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Do something with the form data
     console.log(formData);
   };
 
   const handleRemove = (index) => {
-    dispatch(removeSingleItem(index))
-    console.log("index", index)
+    dispatch(removeSingleItem(index));
+    console.log("index", index);
   }
 
   const handleRemoveAllItem = () => {
-    dispatch(removeAllItem())
+    dispatch(removeAllItem());
   }
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredItems = todoitems.filter((item) =>
+    item.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label>
-          Text Input:
+        {/* Form inputs */}
+      </form>
+
+      <h1>ToDo List</h1>
+      <div className="container" style={{ display: 'flex', flexDirection: 'column' }}>
+        <input
+          type="text"
+          placeholder="Search items"
+          value={searchQuery}
+          onChange={handleSearch}
+          style={{ marginBottom: '10px', padding: '5px' }}
+        />
+        <div style={{ display: 'flex', marginBottom: '10px' }}>
           <input
             type="text"
-            name="textInput"
-            value={formData.textInput}
-            onChange={handleChange}
+            placeholder="Enter the Item"
+            style={{ marginRight: '10px', padding: '5px' }}
+            value={itemvalue}
+            onChange={(event) => setValue(event.target.value)}
           />
-        </label>
-        <br />
-
-        <label>
-          Email Input:
-          <input
-            type="email"
-            name="emailInput"
-            value={formData.emailInput}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Password Input:
-          <input
-            type="password"
-            name="passwordInput"
-            value={formData.passwordInput}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Checkbox Input:
-          <input
-            type="checkbox"
-            name="checkboxInput"
-            checked={formData.checkboxInput}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Radio Input 1:
-          <input
-            type="radio"
-            name="radioInput"
-            value="option1"
-            checked={formData.radioInput === 'option1'}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Radio Input 2:
-          <input
-            type="radio"
-            name="radioInput"
-            value="option2"
-            checked={formData.radioInput === 'option2'}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Select Input:
-          <select name="selectInput" value={formData.selectInput} onChange={handleChange}>
-            <option value="">Select an option</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-          </select>
-        </label>
-        <br />
-
-        <label>
-          Textarea Input:
-          <textarea
-            name="textareaInput"
-            value={formData.textareaInput}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <button type="submit">Submit</button>
-      </form>
-      <h1>ToDo List</h1>
-      <input type="text" style={{ marginTop: '30px' }} value={itemvalue} onChange={(event) => setValue(event.target.value)} />
-      <button onClick={() => handleAddItem()}>Add Item</button>
-      <button onClick={() => handleRemoveAllItem()}>Remove All</button>
-
-      {todoitems.length > 0 && todoitems.map((item, index) => (
-        <div style={{ display: 'flex', flexDirection: 'row' }}><p key={index}>{item}</p><button style={{ width: 'fit-content', height: 'fit-content' }} onClick={() => handleRemove(index)}>RemoveItem</button></div>
-      ))}
-
+          <button
+            style={{ padding: '5px' }}
+            onClick={() => handleAddItem()}
+          >
+            Add Item
+          </button>
+        </div>
+        <button
+          style={{ marginBottom: '10px', padding: '5px' }}
+          onClick={() => handleRemoveAllItem()}
+        >
+          Remove All
+        </button>
+       
+        {filteredItems.length > 0 ? filteredItems.map((item, index) => (
+          <div style={{ display: 'flex', flexDirection: 'row' }} key={index}>
+            <p>{item}</p>
+            <button
+              style={{ width: 'fit-content', height: 'fit-content' }}
+              onClick={() => handleRemove(index)}
+            >
+              Remove Item
+            </button>
+          </div>
+        )) : <p>No item found</p>}
+      </div>
     </>
   );
 };
